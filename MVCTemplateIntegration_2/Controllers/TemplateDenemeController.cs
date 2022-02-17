1d004f64537f71df32acc6335cc4b39c6f0c7038
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2022-02-17 14:10:06 +0300

    Add project files.

---
+++ MVCTemplateIntegration_2/Controllers/TemplateDenemeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTemplateIntegration_2.Controllers
+{
+    public class TemplateDenemeController : Controller
+    {
+        // GET: TemplateDeneme
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCTemplateIntegration_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCTemplateIntegration_2.MvcApplication" Language="C#" %>
